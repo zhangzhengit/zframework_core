0ad378debd54b75e90489db79c063f75aa3f223d
zhangzhen <1071324756@qq.com>
2024-12-08 14:56:41 +0800

    ZArray改进了，运行快很多了 > 解析请求头快很多了

--- zframework_core/src/main/java/com/vo/core/ZArray.java
+++ zframework_core/src/main/java/com/vo/core/ZArray.java
@@ -1,12 +1,7 @@
 package com.vo.core;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.concurrent.atomic.AtomicReference;
-
-import com.google.common.collect.Lists;
-
-import cn.hutool.core.util.ArrayUtil;
+import java.util.List;
 
 /**
  * 动态数组
@@ -17,71 +12,44 @@ import cn.hutool.core.util.ArrayUtil;
  */
 // FIXME 2023年10月26日 下午5:00:58 zhanghen: 这个类里所有方法都太差，需要改进
 public class ZArray {
-//	public static void main(final String[] args) {
-//		final ZArray array = new ZArray();
-//		System.out.println(Arrays.toString(array.get()));
-//		array.add(new byte[] { 1, 2, 3, 4, 5, });
-//		System.out.println(Arrays.toString(array.get()));
-//		array.add(new byte[] { 6 });
-//		System.out.println(Arrays.toString(array.get()));
-//		array.remove(new byte[] { 4, 5 });
-//		System.out.println(Arrays.toString(array.get()));
-//	}
 
-	private final AtomicReference<byte[]> ar = new AtomicReference<>(new byte[] {});
+	private final List<Byte> ar = new ArrayList<>();
 
 	public ZArray() {
 
 	}
 
 	public ZArray(final byte[] ba) {
-		this.ar.set(ba);
-	}
-
-	public byte[] add(final byte[] ba, final int from, final int to) {
-		final byte[] add = Arrays.copyOfRange(ba, from, to);
-		if (this.ar.get().length <= 0) {
-			this.ar.set(add);
-		} else {
-			this.ar.set(ArrayUtil.addAll(this.ar.get(), add));
-		}
-		return this.ar.get();
-	}
-
-	public byte[] add(final byte[] ba) {
-		this.ar.set(ArrayUtil.addAll(this.ar.get(), ba));
-		return this.ar.get();
-	}
-
-	public void remove(final byte[] ba) {
-		final ArrayList<Byte> newArrayList = Lists.newArrayListWithCapacity(this.ar.get().length);
-		final byte[] aaa = this.ar.get();
-		for (final byte b : aaa) {
-			newArrayList.add(b);
-		}
-
-		final ArrayList<Byte> aaaAAA = Lists.newArrayListWithCapacity(ba.length);
 		for (final byte b : ba) {
-			aaaAAA.add(b);
+			this.ar.add(b);
 		}
+	}
 
-		newArrayList.removeAll(aaaAAA);
-
-		final byte[] r = new byte[newArrayList.size()];
-		int n = 0;
-		for (final byte b : newArrayList) {
-			r[n++] = b;
+	public void add(final byte[] ba, final int from, final int to) {
+		for (int i = from; i < to; i++) {
+			this.ar.add(ba[i]);
 		}
-		this.ar.set(r);
+	}
 
+	public void add(final byte[] ba) {
+		this.add(ba, 0, ba.length);
 	}
 
 	public byte[] get() {
-		return this.ar.get();
+		if (this.ar.isEmpty()) {
+			return new byte[] {};
+		}
+
+		final byte[] ba = new byte[this.ar.size()];
+		for (int i = 0; i < this.ar.size(); i++) {
+			ba[i] = this.ar.get(i);
+		}
+
+		return ba;
 	}
 
 	public void clear() {
-		this.ar.set(new byte[0]);
+		this.ar.clear();
 	}
 
 }
