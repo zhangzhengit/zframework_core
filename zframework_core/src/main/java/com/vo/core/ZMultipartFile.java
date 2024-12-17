a0a561957232ba71bf8553a1cbf8ceaf05f14f0e
zhangzhen <1071324756@qq.com>
2024-12-17 17:03:32 +0800

    删除了一个 2023年10月26日 的TODO： ZMultipartFile处理上传文件时image和txt的乱码问题，这个最近几天解决了。OK

--- zframework_core/src/main/java/com/vo/core/ZMultipartFile.java
+++ zframework_core/src/main/java/com/vo/core/ZMultipartFile.java
@@ -16,17 +16,6 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-// FIXME 2023年10月26日 下午10:05:48 zhanghen: 这个类支持还有问题，
-// 1、现在使用 http 完整信息读取为byte[] 然后整个转为String解析
-/*	问题：NioLongConnectionServer.CHARSET 使用default（UTF-8）,上传txt文件正常。上传其他文件如jpg不支持
- * 		NioLongConnectionServer.CHARSET 使用 ISO-8859-1，上传jpg文件正常，但文件名显示不了中文。
- * 	TODO ：
- * 			1 byte[] 一边解析，一边看Content-Type，如果form-data，则再看
- * 				body的 Content-type 来处理：原始byte[] remove掉已转为String的byte[]和
- * 						boundary，剩余的就是文件的byte[]?
- *
- *
- */
 public class ZMultipartFile {
 
 	private String name;
@@ -34,10 +23,6 @@ public class ZMultipartFile {
 	private byte[] content;
 	private String contentType;
 	private InputStream inputStream;
-
-//	public String getName() {
-//		return null;
-//	}
 
 	/**
 	 * 返回上传的文件的名称，指文件名
