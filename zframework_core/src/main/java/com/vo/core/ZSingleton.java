39adb638a4491cc4c31dd1fdc8b1140e4c727ebe
zhangzhen <1071324756@qq.com>
2024-12-23 22:16:26 +0800

    getCanonicalName 改为getName

--- zframework_core/src/main/java/com/vo/core/ZSingleton.java
+++ zframework_core/src/main/java/com/vo/core/ZSingleton.java
@@ -17,17 +17,17 @@ public class ZSingleton {
 	private static final ConcurrentMap<String, Object> SINGLETON_MAP = Maps.newConcurrentMap();
 
 	public static Object getSingletonByClassName(final String clsName) {
-		final String key = clsName;
-		synchronized (key) {
-			final Object v = SINGLETON_MAP.get(key);
-			if (v != null) {
-				return v;
-			}
+		final Object v = SINGLETON_MAP.get(clsName);
+		if (v != null) {
+			return v;
+		}
+
+		synchronized (clsName) {
 
 			try {
 				final Class<?> cls = Class.forName(clsName);
 				final Object newInstance = cls.newInstance();
-				SINGLETON_MAP.put(key, newInstance);
+				SINGLETON_MAP.put(clsName, newInstance);
 				return newInstance;
 			} catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
 				e.printStackTrace();
@@ -39,24 +39,19 @@ public class ZSingleton {
 
 	public static <T> T getSingletonByClass(final Class<T> cls) {
 
-		// FIXME 2024年12月22日 下午9:35:14 zhangzhen : 所有的getCanonicalName都想办法替换掉，因为太慢了
-		// 1 getCanonicalName 太慢了
-		final String key = cls.getCanonicalName();
+		final Object v = SINGLETON_MAP.get(cls.getName());
+		if (v != null) {
+			return (T) v;
+		}
 
-		synchronized (key) {
-
-			final Object v = SINGLETON_MAP.get(key);
-			if (v != null) {
-				return (T) v;
-			}
+		synchronized (cls.getName()) {
 			try {
 				final Object newInstance = cls.newInstance();
-				SINGLETON_MAP.put(key, newInstance);
+				SINGLETON_MAP.put(cls.getName(), newInstance);
 				return (T) newInstance;
 			} catch (InstantiationException | IllegalAccessException e) {
 				e.printStackTrace();
 			}
-
 			return null;
 		}
 	}
