06c261c081f5166b897c89600eda7ea0f3524f4d
zhangzhen <1071324756@qq.com>
2025-01-14 12:58:05 +0800

    配置项：[application.event.thread.count] 默认值改为 Math.min(10, Runtime.getRuntime().availableProcessors())，new此线程池改为延迟new Thread

--- zframework_core/src/main/java/com/vo/scanner/ZApplicationEventConfiguration.java
+++ zframework_core/src/main/java/com/vo/scanner/ZApplicationEventConfiguration.java
@@ -4,6 +4,7 @@ import com.vo.anno.ZAutowired;
 import com.vo.anno.ZBean;
 import com.vo.anno.ZConfiguration;
 import com.vo.core.ZLog2;
+import com.votool.ze.ThreadModeEnum;
 import com.votool.ze.ZE;
 import com.votool.ze.ZES;
 
@@ -17,17 +18,12 @@ import com.votool.ze.ZES;
 @ZConfiguration
 public class ZApplicationEventConfiguration {
 
-	private static final ZLog2 LOG = ZLog2.getInstance();
-
 	@ZAutowired
 	private ZApplicationEventConfigurationProperties applicationEventConfigurationProperties;
 
 	@ZBean
 	public ZE zeForApplicationEventPublisher() {
-		//		LOG.info("事件机制配置类开始初始化,{}={}", this.applicationEventConfigurationProperties.getClass().getSimpleName(),
-		//				this.applicationEventConfigurationProperties);
-
 		return ZES.newZE(this.applicationEventConfigurationProperties.getThreadCount(),
-				this.applicationEventConfigurationProperties.getThreadNamePrefix());
+				this.applicationEventConfigurationProperties.getThreadNamePrefix(), ThreadModeEnum.LAZY);
 	}
 }
--- zframework_core/src/main/java/com/vo/scanner/ZApplicationEventConfigurationProperties.java
+++ zframework_core/src/main/java/com/vo/scanner/ZApplicationEventConfigurationProperties.java
@@ -29,7 +29,7 @@ public class ZApplicationEventConfigurationProperties {
 	@ZNotNull
 	@ZMin(min = 1)
 	@ZMax(max = 100)
-	private Integer threadCount = 4;
+	private Integer threadCount = Math.min(10, Runtime.getRuntime().availableProcessors());
 
 	@ZNotEmtpy
 	private String threadNamePrefix = "applicationEvent-Thread-";
