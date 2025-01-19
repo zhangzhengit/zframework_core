9a8e5e2ce49882d5451a00ba45a2a08fe2463680
zhangzhen <1071324756@qq.com>
2025-01-19 19:56:07 +0800

    ZMailValidator 抛出的ValidatedException也加入了httpStatus = 400

--- zframework_core/src/main/java/com/vo/email/ZMailValidator.java
+++ zframework_core/src/main/java/com/vo/email/ZMailValidator.java
@@ -6,6 +6,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import com.vo.exception.ValidatedException;
+import com.vo.http.HttpStatusEnum;
 import com.vo.validator.ZCustomValidator;
 
 /**
@@ -34,7 +35,7 @@ public class ZMailValidator implements ZCustomValidator {
 			if (!matcher.matches()) {
 				final String message = object.getClass().getSimpleName() + "." + field.getName() + "值[" + email
 						+ "]不符合邮箱地址规则";
-				throw new ValidatedException(message);
+				throw new ValidatedException(message, HttpStatusEnum.HTTP_400.getCode());
 			}
 		} else if (email instanceof Set) {
 			final Set set = (Set) email;
@@ -43,7 +44,7 @@ public class ZMailValidator implements ZCustomValidator {
 				if (!matcher.matches()) {
 					final String message = object.getClass().getSimpleName() + "." + field.getName() + "值[" + e
 							+ "]不符合邮箱地址规则";
-					throw new ValidatedException(message);
+					throw new ValidatedException(message, HttpStatusEnum.HTTP_400.getCode());
 				}
 			}
 		}
