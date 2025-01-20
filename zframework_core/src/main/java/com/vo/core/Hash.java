a1f10df11126dc387d73556f5c4028b9c24f8cfa
zhangzhen <1071324756@qq.com>
2025-01-20 18:52:12 +0800

    Hash 类新增几个hash方法

--- zframework_core/src/main/java/com/vo/core/Hash.java
+++ zframework_core/src/main/java/com/vo/core/Hash.java
@@ -12,15 +12,15 @@ import com.google.common.hash.Hashing;
  */
 public class Hash {
 
-	public static String c(final String string) {
-		return c(string.getBytes());
+	public static String murmur3(final String string) {
+		return murmur3(string.getBytes());
 	}
 
-	public static String c(final byte[] ba) {
+	public static String murmur3(final byte[] ba) {
 		return Hashing.murmur3_128().newHasher().putBytes(ba).hash().toString();
 	}
 
-	public static String c(final List<Byte> bl) {
+	public static String murmur3(final List<Byte> bl) {
 		if ((bl == null) || bl.isEmpty()) {
 			return "";
 		}
@@ -29,6 +29,19 @@ public class Hash {
 		for (int i = 0; i < bl.size(); i++) {
 			b[i] = bl.get(i);
 		}
-		return c(b);
+		return murmur3(b);
 	}
+
+	public static String md5(final byte[] ba) {
+		return Hashing.md5().newHasher().putBytes(ba).hash().toString();
+	}
+
+	public static String sha256(final byte[] ba) {
+		return Hashing.sha256().newHasher().putBytes(ba).hash().toString();
+	}
+
+	public static String goodFastHash(final byte[] ba) {
+		return Hashing.goodFastHash(256).newHasher().putBytes(ba).hash().toString();
+	}
+
 }
