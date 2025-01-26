87a79e2bb39da34937152c83cf2724343287f61c
zhangzhen <1071324756@qq.com>
2025-01-26 19:53:45 +0800

    删除已完成的TODO： @ZValidated 可用在class上，然后class作为方法参数，调用方法时可以自动校验此class字段上的ZNotNull等注解了

--- zframework_core/src/main/java/com/vo/validator/ZValidated.java
+++ zframework_core/src/main/java/com/vo/validator/ZValidated.java
@@ -6,16 +6,31 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-// FIXME 2024年2月4日 下午5:40:19 zhanghen: 2 貌似还有问题，暂时不要用方式2
 /**
  * 表示启用校验（校验@ZNotNull等用在字段上的注解）。
  * 支持：
  * 	1、接口方法参数的请求对象上，如：
- * 		public CR buildstart(@ZRequestBody @ZValidated final BuildDTO buildDTO)
+ * 	
+ * 		@ZRequestMapping(mapping = { "/test" })
+ * 		public void test(@ZRequestBody @ZValidated final BuildDTO buildDTO){
+ * 			// 在执行业务代码前，如果 BuildDTO 中的字段带有 @ZNotNull  @ZNotEmtpy 等等注解,
+ *			// 则自动校验，通过则继续执行下面的业务代码，未通过则会自动抛出异常 
+ * 				
+ * 			// 业务代码
+ * 		}
+ * 		
+ * 
  * 	2、@ZComponent 对象的方法的参数上，如：
- * 		public void zv2(final ZVDTO zvdto)
- *	   其中 ZVDTO类上加入本注解，则在接口中调用 zv2方法时会自动校验zvdto上的字段。
- *
+ * 
+ * 		public void zv(final ZVDTO zvdto) {
+ * 			// 业务代码
+ * 		}
+ *	   	
+ *		// 如上代码，如果 ZVDTO 在类上带有本注解，则在调用zv()方法时，会自动校验
+ *		// @ZNotNull  @ZNotEmtpy 等等注解
+ *		// 无则不校验，继续执行
+ *		// 有则校验，通过则继续执行，未通过则抛出异常
+ *		
  *
  * @author zhangzhen
  * @date 2023年10月15日
