297afbaf5d367be7b7a78b7e4346fb230656717d
zhangzhen <1071324756@qq.com>
2025-01-26 20:00:50 +0800

    删除不打算做的TODO： 在一个字段上用多个校验注解，可能会出现不合理的情况， 如：最大值=5，最小值=8，可以正确编译，但是运行时不管怎么传值都会报错。想在启动时进行校验来禁止这种情况的话太复杂了，要考虑的情况太多了。就不校验了，让这种情况在测试时被发现吧。

--- zframework_core/src/main/java/com/vo/validator/ZValidator.java
+++ zframework_core/src/main/java/com/vo/validator/ZValidator.java
@@ -574,13 +574,6 @@ public class ZValidator {
 	}
 
 	public static void validatedAll(final Object object, final Field field) {
-		// FIXME 2023年10月31日 下午10:13:39 zhanghen: 启动时是否验证注解值的合理性，如下声明：
-		// @ZStartWith(prefix = "ZH")
-		// @ZEndsWith(suffix = "G")
-		// @ZLength(max = 3)
-		// private String name;
-
-		// 显然是不合理，name怎么传值都不会通过验证。该怎么办？程序启动时就验证所有的注解的组合中不合理的情况？
 
 		ZValidator.validatedZNotNull(object, field);
 		ZValidator.validatedZNotEmpty(object, field);
@@ -598,8 +591,7 @@ public class ZValidator {
 	private static Object getFieldValue(final Object object, final Field field) {
 		try {
 			field.setAccessible(true);
-			final Object v = field.get(object);
-			return v;
+			return field.get(object);
 		} catch (IllegalArgumentException | IllegalAccessException e) {
 			e.printStackTrace();
 		}
