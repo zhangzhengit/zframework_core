6a0ec52419c3541dd8e023b0efb5da9cedc60d6c
zhangzhen <1071324756@qq.com>
2025-11-26 12:16:22 +0800

    修改 ZTemplate 因缓存而导致占用大量内存的问题

--- zframework_core/src/main/java/com/vo/template/ZTemplate.java
+++ zframework_core/src/main/java/com/vo/template/ZTemplate.java
@@ -25,7 +25,8 @@ public class ZTemplate {
 	private static final Configuration CFG = new Configuration();
 
 	/**
-	 * 从一个带有freemarker标签的html文档的字符串形式，处理其中的freemarker标签，而生成一个完整的可以被浏览器直接解析的html文档。
+	 * 从一个带有freemarker标签的html文档的字符串形式，
+	 * 处理其中的freemarker标签，而生成一个完整的可以被浏览器直接解析的html文档。
 	 *
 	 * @param htmlContent
 	 * @return
@@ -46,41 +47,52 @@ public class ZTemplate {
 	private static String freemarker0(final String templateString) {
 		final Map<String, Object> dataModel = ZModel.get();
 
-		final String key =
-				dataModel != null ?
-						("f0-" + dataModel.hashCode()
-						+ "-" + dataModel.size()
-						+ "-" + templateString) :
-							templateString;
+		// FIXME 2025年11月26日 12:03:26 zhangzhen :  暂时的修改：
+		// 1的方式对于动态生成的页面，如带有时间戳的页面，会一直缓存导致浪费大量内存
+		// 暂时想到的办法：
+		// 1 加入缓存过期策略，似乎也在大多场景下没啥意义，因为缓存复用的可能不太大
+		// 2 如下方式，直接不用缓存
+		try (StringWriter writer = new StringWriter()) {
+			final Template template = getTemplateCache(templateString);
 
-		final String v = ZRC.computeIfAbsent("freemarker0" + '-' + key, () ->{
-
-			try {
-				final Template template = getTemplate(templateString);
-
-				final StringWriter writer = new StringWriter();
-
-				template.process(dataModel, writer);
-				final String output = writer.toString();
-				return output;
-			} catch (IOException | TemplateException e) {
-				e.printStackTrace();
-			}
-
-			return null;
-		});
-
-		return v;
+			template.process(dataModel, writer);
+			return writer.toString();
+		} catch (IOException | TemplateException e) {
+			e.printStackTrace();
+		}
+		return null;
+		
+		// 1 用cache，导致占了太多内存了
+//		final String key =
+//				dataModel != null ?
+//						("f0-" + dataModel.hashCode()
+//						+ "-" + dataModel.size()
+//						+ "-" + templateString) :
+//							templateString;
+//
+//		return ZRC.computeIfAbsent("freemarker0" + '-' + key, () ->{
+//
+//			try (StringWriter writer = new StringWriter()) {
+//				final Template template = getTemplateCache(templateString);
+//
+//				template.process(dataModel, writer);
+//				return writer.toString();
+//			} catch (IOException | TemplateException e) {
+//				e.printStackTrace();
+//			}
+//
+//			return null;
+//		});
 	}
 
-	private static Template getTemplate(final String templateString) throws IOException {
-		return ZRC.computeIfAbsent("getTemplate" + '-' + templateString, supplier(templateString));
+	private static Template getTemplateCache(final String templateString) throws IOException {
+		return ZRC.computeIfAbsent("fT-" + templateString, supplier(templateString));
 	}
 
 	private static Supplier<Template> supplier(final String templateString) {
 		return () -> {
 			try {
-				return new Template("template-" + templateString.hashCode(), templateString, CFG);
+				return new Template("fT-" + templateString.hashCode(), templateString, CFG);
 			} catch (final IOException e) {
 				e.printStackTrace();
 			}
