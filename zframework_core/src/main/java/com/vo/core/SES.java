bc4888f6db7ecd2a672194b94d1358e93b32452c
zhangzhen <1071324756@qq.com>
2026-04-30 18:03:36 +0800

    新增SES枚举（上次提交忘了的）

---
+++ zframework_core/src/main/java/com/vo/core/SES.java
@@ -0,0 +1,17 @@
+package com.vo.core;
+
+/**
+ * @ZSynchronously 事件状态
+ *
+ * @author zhangzhen
+ * @date 2026年4月30日 17:17:56
+ */
+public enum SES {
+
+	DAIZHIXING,
+
+	ZHIXINGZHONG,
+
+	ZHIXINGJIESHU;
+
+}
