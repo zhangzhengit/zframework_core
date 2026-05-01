5d2b1c7881227ecb3282df15e0ce42d37120e404
zhangzhen <1071324756@qq.com>
2026-05-01 14:55:37 +0800

    @SyncAOP 类修改： 1、sync改为了 @ZSync指定的值作为key ，然后sync(key.intern()) 2、由于1，去掉了虚拟线程池，直接在当前线程执行就行了，上次提交是错误的测试，测试使用串行了只简单看了一下就提交了

--- zframework_core/src/main/java/com/vo/core/ZSynchronouslyuAOP.java
+++ zframework_core/src/main/java/com/vo/core/ZSynchronouslyuAOP.java
@@ -4,12 +4,6 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 import com.vo.aop.AOPParameter;
 import com.vo.aop.ZAOP;
@@ -27,9 +21,6 @@ import com.vo.http.ZSynchronously;
 @ZAOP(interceptType = ZSynchronously.class)
 public class ZSynchronouslyuAOP implements ZIAOP {
 
-	private final Map<String, SES> map = new ConcurrentHashMap<>();
-	private final ExecutorService ve = Executors.newVirtualThreadPerTaskExecutor();
-
 	@Override
 	public Object before(final AOPParameter AOPParameter) {
 		return null;
@@ -37,47 +28,14 @@ public class ZSynchronouslyuAOP implements ZIAOP {
 
 	@Override
 	public Object around(final AOPParameter AOPParameter) {
-		// FIXME 2026年4月30日 17:51:49 zhangzhen : 先暂时测了一下没问题，还要继续测
-		synchronized (this) {
 
-			final String value = ZSynchronouslyuAOP.gValue(AOPParameter);
-			this.map.put(value, SES.DAIZHIXING);
-
-			final SES ses = this.map.get(value);
-			if ((ses == null) || (ses == SES.DAIZHIXING)) {
-				return this.invoke(AOPParameter, value);
-			}
-
-			if (this.map.get(value) == SES.ZHIXINGZHONG) {
-				try {
-					Thread.sleep(1);
-				} catch (final InterruptedException e) {
-					e.printStackTrace();
-				}
-
-				return this.invoke(AOPParameter, value);
-			}
-
-		}
-
-		return null;
-	}
-
-	private Object invoke(final AOPParameter AOPParameter, final String value) {
-		this.map.put(value, SES.ZHIXINGZHONG);
-		final Future<Object> future = this.ve.submit(() -> {
+		final String value = ZSynchronouslyuAOP.gValue(AOPParameter);
+		
+		synchronized (("AOPLock" + value).intern()) {
 			final Object v = AOPParameter.invoke();
-			ZSynchronouslyuAOP.this.map.put(value, SES.DAIZHIXING);
 			return v;
-		});
-
-		try {
-			return future.get();
-		} catch (InterruptedException | ExecutionException e) {
-			e.printStackTrace();
 		}
-
-		return null;
+		
 	}
 
 	private static String gValue(final AOPParameter AOPParameter) {
