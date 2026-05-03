08cb48fb13dbe9b797a6db35e61f430387e5fbfc
zhangzhen <1071324756@qq.com>
2026-05-03 11:23:01 +0800

    添加TODO：@ZMax 也要和@ZMin 一样支持 用在@ZPathVariable 的字段上

--- zframework_core/src/main/java/com/vo/validator/ZMax.java
+++ zframework_core/src/main/java/com/vo/validator/ZMax.java
@@ -6,6 +6,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import com.vo.core.ZPathVariable;
+
 /**
  *
  * 放在字段上，表示此字段最小值不能比指定的值大，用于数值类型上，所有  extends Number 的类型。
@@ -19,6 +21,8 @@ import java.lang.annotation.Target;
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
+// FIXME 2026年5月3日 11:06:05 zhangzhen : 这个也要支持和ZMin一样
+// 如：@ZPathVariable @ZMin(min = 100) @ZMax(max = 10000 * 3) final Integer count
 @Target({ ElementType.FIELD })
 public @interface ZMax {
 
