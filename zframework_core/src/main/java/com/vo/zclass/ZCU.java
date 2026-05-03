a7d62b315ade33f8360bce2fc6549af91067764c
zhangzhen <1071324756@qq.com>
2026-05-03 11:32:52 +0800

    class.newInstance 改为 constructor.newInstance

--- zframework_core/src/main/java/com/vo/zclass/ZCU.java
+++ zframework_core/src/main/java/com/vo/zclass/ZCU.java
@@ -1,5 +1,7 @@
 package com.vo.zclass;
 
+import java.lang.reflect.InvocationTargetException;
+
 import org.codehaus.commons.compiler.CompileException;
 import org.codehaus.janino.SimpleCompiler;
 
@@ -27,15 +29,16 @@ public class ZCU {
 	public static Object newInstance(final String source, final String package1, final String className) {
 		final SimpleCompiler compiler = new SimpleCompiler();
 		try {
+
 			compiler.cook(source);
-			
 
 			final String cN = package1.substring(package1.indexOf(PACKAGE) + PACKAGE.length()) + "." + className;
 			final Class<?> cls = compiler.getClassLoader().loadClass(cN);
-			final Object newInstance = cls.newInstance();
+			final Object newInstance = cls.getDeclaredConstructor().newInstance();
 
 			return newInstance;
-		} catch (final CompileException | ClassNotFoundException | InstantiationException | IllegalAccessException e1) {
+
+		} catch (final CompileException | ClassNotFoundException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e1) {
 			e1.printStackTrace();
 		}
 
