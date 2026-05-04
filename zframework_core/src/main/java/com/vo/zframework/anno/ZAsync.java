ecc78b53627de71b02f11aba8e22d898705be004
zhangzhen <1071324756@qq.com>
2026-05-04 18:41:18 +0800

    @ZAsync 新增功能： 1、支持了被标记的方法带有返回值 返回ZAsyncRV，使用 ZAsyncRV.ok 构造返回值 2、启动流程中加入了此注解标记方法的校验，校验内容见1

--- zframework_core/src/main/java/com/vo/zframework/anno/ZAsync.java
+++ zframework_core/src/main/java/com/vo/zframework/anno/ZAsync.java
@@ -8,6 +8,12 @@ import java.lang.annotation.Target;
 
 /**
  * 用在方法上，表示此方法异步执行
+ * 带有本注解的方法必须声明为public修饰
+ *
+ * 对于无返回值的方法，正常写即可；
+ *
+ * 对于有返回值的方法，必须使用 ZAsyncRV<T> 作为返回类型，
+ * 		并且使用 ZAsyncRV.ok(T v) 来构造返回值
  *
  * @author zhangzhen
  * @date 2023年7月8日
