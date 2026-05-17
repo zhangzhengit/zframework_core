f7d64b1f3d13da410550e03cd516e759afc8a76c
zhangzhen <1071324756@qq.com>
2026-05-17 16:58:21 +0800

    com.vo.zframework.common.CR 添加toString

--- zframework_core/src/main/java/com/vo/zframework/common/CR.java
+++ zframework_core/src/main/java/com/vo/zframework/common/CR.java
@@ -9,10 +9,10 @@ import com.vo.zframework.enums.ErrorEnum;
  * 统一返回
  *
  * @param <T>
- * 
+ *
  * @author zhangzhen
  * @date 2020-12-08 13:41:18
- * 
+ *
  */
 public class CR<T> implements Serializable {
 
@@ -29,22 +29,22 @@ public class CR<T> implements Serializable {
 	private T data;
 
 	public boolean isOk() {
-		return this.getCode() == CODE_OK || this.getCode() == CODE_OK_REDIRECT;
+		return (this.getCode() == CODE_OK) || (this.getCode() == CODE_OK_REDIRECT);
 	}
-	
-	public CR<T> okRedirect(String redirectURL) {
+
+	public CR<T> okRedirect(final String redirectURL) {
 		this.setRedirectURL(redirectURL);
 		return this;
 	}
-	
-	
+
+
 	public static <T> CR<T> ok() {
 		final CR<T> cr = new CR<>();
 		cr.setCode(CODE_OK);
 		return cr;
 	}
-	
-	public static <T> CR<T> okMessage(String message) {
+
+	public static <T> CR<T> okMessage(final String message) {
 		final CR<T> cr = new CR<>();
 		cr.setCode(CODE_OK);
 		cr.setMessage(message);
@@ -58,7 +58,7 @@ public class CR<T> implements Serializable {
 		return cr;
 	}
 
-	public static <T> CR<T> error(Integer code, String message) {
+	public static <T> CR<T> error(final Integer code, final String message) {
 		final CR<T> cr = new CR<>();
 		cr.setCode(code);
 		cr.setMessage(message);
@@ -73,47 +73,50 @@ public class CR<T> implements Serializable {
 	}
 
 	public int getCode() {
-		return code;
+		return this.code;
 	}
 
-	public void setCode(int code) {
+	public void setCode(final int code) {
 		this.code = code;
 	}
 
 	public String getMessage() {
-		return message;
+		return this.message;
 	}
 
-	public void setMessage(String message) {
+	public void setMessage(final String message) {
 		this.message = message;
 	}
 
 	public String getRedirectURL() {
-		return redirectURL;
+		return this.redirectURL;
 	}
 
-	public void setRedirectURL(String redirectURL) {
+	public void setRedirectURL(final String redirectURL) {
 		this.redirectURL = redirectURL;
 	}
 
 	public T getData() {
-		return data;
+		return this.data;
 	}
 
-	public void setData(T data) {
+	public void setData(final T data) {
 		this.data = data;
 	}
 
-	public CR(int code, String message, String redirectURL, T data) {
-		super();
+	public CR(final int code, final String message, final String redirectURL, final T data) {
 		this.code = code;
 		this.message = message;
 		this.redirectURL = redirectURL;
 		this.data = data;
 	}
-	
+
 	public CR() {
 	}
-	
+
+	@Override
+	public String toString() {
+		return "CR [code=" + this.code + ", message=" + this.message + ", data=" + this.data + ", redirectURL=" + this.redirectURL + "]";
+	}
 
 }
