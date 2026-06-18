d8d56056424279ca73dd19994c107e280f03d52c
zhangzhen <1071324756@qq.com>
2026-06-19 07:09:32 +0800

    if (!response.containsHeader(HeaderEnum.ETAG.getName())) {添加注释：// 在此判断不包含ETag头，意思是没有手动设置过，则在此自动设置

--- zframework_core/src/main/java/vo/zframework/core/HTTPResponseProcessor.java
+++ zframework_core/src/main/java/vo/zframework/core/HTTPResponseProcessor.java
@@ -87,6 +87,7 @@ public class HTTPResponseProcessor {
 
 			final int httpStatus = response.getHttpStatus();
 			if (httpStatus == HttpStatusEnum.HTTP_200.getStatus()) {
+				// 在此判断不包含ETag头，意思是没有手动设置过，则在此自动设置
 				if (!response.containsHeader(HeaderEnum.ETAG.getName())) {
 					response.setETagIfZETagPresent(request, response.getBody(), ETagEnum.STRONG);
 				}
