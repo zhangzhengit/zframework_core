d1e4b723a67429a5866b90ad0b914c870f510ffe
zhangzhen <1071324756@qq.com>
2026-06-24 20:07:55 +0800

    @ZOrder维护javadoc

--- zframework_core/src/main/java/vo/zframework/anno/ZOrder.java
+++ zframework_core/src/main/java/vo/zframework/anno/ZOrder.java
@@ -7,7 +7,8 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * 表示多个bean的执行顺序，从小到大执行，取值为int范围。值越小越小执行，Integer.MIN_VALUE 为最高优先级
+ * 表示多个bean的执行顺序，从小到大执行，取值为int范围
+ * 值越小越早执行，Integer.MIN_VALUE 为最高优先级
  *
  * @author zhangzhen
  * @date 2023年7月11日
@@ -18,8 +19,11 @@ import java.lang.annotation.Target;
 @Target({ ElementType.TYPE })
 public @interface ZOrder {
 
+	/**
+	 * 指定一个int值，越小越早执行
+	 *
+	 * @return
+	 */
 	int value();
-
-
 
 }
