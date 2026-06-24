e529b815d7ef1bcf44dc61365405e1d6fb59d45a
zhangzhen <1071324756@qq.com>
2026-06-24 16:56:22 +0800

    添加TODO：在去掉pom的各个依赖和解决打出jar过大的问题，想只保留提供rest接口的部分 ，在去掉zstd时测试aircom有点慢，暂时zstd不改

--- zframework_core/src/main/java/vo/zframework/compression/ZSTD.java
+++ zframework_core/src/main/java/vo/zframework/compression/ZSTD.java
@@ -1,13 +1,11 @@
 package vo.zframework.compression;
 
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.Charset;
+import java.nio.ByteBuffer;
 import java.util.Arrays;
 
 import com.github.luben.zstd.Zstd;
 
 import vo.zframework.common.AU;
-import vo.zframework.common.STU;
 
 /**
  *
@@ -17,43 +15,46 @@ import vo.zframework.common.STU;
  * @date 2025年1月2日 下午8:37:12
  *
  */
+// FIXME 2026年6月24日 16:48:56 zhangzhen : zstd用jni默认打了所有平台的库，jar太大了，试了aircompressor,
+/*	简单测试下了，在debian上openjdk25 p8600 4GB thinkpadx200
+ *  启动两个jar，唯一不同A用jniB用air，ab -H "Accept-Encoding: zstd"而是，前者qps在5600左右，后者只有4100左右
+ *	所以要想jar小，要在打包前修改pom。本次想改zstd是按打出的jar中的lib中的jar大小排序来改的，
+ *  不只是想去掉zstd，包括jedis freemarker都要去掉，提供出来一个starter，本工程只提供最简单的rest接口的
+ *
+ *  aircompressor :
+ *
+ * 	<dependency>
+			<groupId>io.airlift</groupId>
+			<artifactId>aircompressor</artifactId>
+			<version>0.21</version>
+		</dependency>
+
+			public static byte[] compress(final byte[] original) {
+
+		final int maxLen = compressor.maxCompressedLength(original.length);
+
+		final ByteBuffer result = ByteBuffer.allocate(maxLen);
+		compressor.compress(ByteBuffer.wrap(original), result);
+
+		result.flip();
+
+		final byte[] compressed = new byte[result.remaining()];
+		result.get(compressed);
+
+		return compressed;
+	}
+ *
+ */
 public class ZSTD {
 
-	public static byte[] decompression(final byte[] ba) {
-		if (AU.isEmpty(ba)) {
-			return null;
-		}
-
-		final long decompressedSize = Zstd.decompressedSize(ba);
-
-		final byte[] buffer = new byte[(int) decompressedSize];
-
-		final long decompress = Zstd.decompress(buffer, ba);
-		return Arrays.copyOf(buffer, (int) decompress);
-	}
+	private static final int DEFALUT_COMPRESS_LEVEL = 3;
 
 	public static byte[] compress(final byte[] ba) {
 		if (AU.isEmpty(ba)) {
 			return null;
 		}
 
-		return Zstd.compress(ba,3);
+		return Zstd.compress(ba,DEFALUT_COMPRESS_LEVEL);
 	}
-
-	public static byte[] compress(final String string) {
-		if (STU.isNullOrEmpty(string)) {
-			return null;
-		}
-
-		byte[] ba = null;
-		try {
-			ba = string.getBytes(Charset.defaultCharset().displayName());
-		} catch (final UnsupportedEncodingException e1) {
-			e1.printStackTrace();
-		}
-
-		return compress(ba);
-	}
-
 
 }
