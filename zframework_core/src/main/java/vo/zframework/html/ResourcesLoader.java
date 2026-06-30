33767cccc371518b1bd1961ce67b51f57d9af22e
zhangzhen <1071324756@qq.com>
2026-06-30 18:55:17 +0800

    ResourceNotExistException 提示信息重复了，现在改好了，不重复了

--- zframework_core/src/main/java/vo/zframework/html/ResourcesLoader.java
+++ zframework_core/src/main/java/vo/zframework/html/ResourcesLoader.java
@@ -62,7 +62,7 @@ public class ResourcesLoader {
 		try {
 			fileReader = new FileReader(name);
 		} catch (final FileNotFoundException e1) {
-			throw new ResourceNotExistException("资源不存在,name = " + resourceName, HttpStatusEnum.HTTP_404.getStatus());
+			throw new ResourceNotExistException(resourceName, HttpStatusEnum.HTTP_404.getStatus());
 		}
 
 		final BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -117,7 +117,7 @@ public class ResourcesLoader {
 			final FIS fis = new FIS(fileInputStream, file);
 			return fis;
 		} catch (final FileNotFoundException e1) {
-			throw new ResourceNotExistException("资源不存在,name = " + resourceName, HttpStatusEnum.HTTP_404.getStatus());
+			throw new ResourceNotExistException(resourceName, HttpStatusEnum.HTTP_404.getStatus());
 		}
 	}
 
@@ -145,7 +145,7 @@ public class ResourcesLoader {
 		try {
 			fileInputStream = new FileInputStream(new File(fileName));
 		} catch (final FileNotFoundException e1) {
-			throw new ResourceNotExistException("资源不存在,name = " + resourceName, HttpStatusEnum.HTTP_404.getStatus());
+			throw new ResourceNotExistException(resourceName, HttpStatusEnum.HTTP_404.getStatus());
 		}
 
 		final byte[] byteArray = readByteArray0(fileInputStream);
@@ -266,7 +266,7 @@ public class ResourcesLoader {
 
 		if (inputStream == null) {
 			// FIXME 2025年12月8日 17:51:22 zhangzhen : 这里提示详细一点，具体时候那个资源
-			throw new ResourceNotExistException("资源不存在:" + resourceName, HttpStatusEnum.HTTP_404.getStatus());
+			throw new ResourceNotExistException(resourceName, HttpStatusEnum.HTTP_404.getStatus());
 		}
 		return new FIS(inputStream, staticFile);
 	}
