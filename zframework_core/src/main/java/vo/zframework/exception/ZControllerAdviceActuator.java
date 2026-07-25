0387d8a32535fb6b796d76fe9d6e9bef0a3bfb76
zhangzhen <1071324756@qq.com>
2026-07-26 02:13:46 +0800

    移除邮件通知功能： 1、这应该是个过度设计，并且现在已经有了统一的异常通知点了，用户通知邮件通知的话自己写代码处理即可，或者提供一个邮件的starter？

--- zframework_core/src/main/java/vo/zframework/exception/ZControllerAdviceActuator.java
+++ zframework_core/src/main/java/vo/zframework/exception/ZControllerAdviceActuator.java
@@ -1,20 +1,12 @@
 package vo.zframework.exception;
 
 import java.lang.reflect.InvocationTargetException;
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.UUID;
 
 import vo.log.core.ZLog2;
-import vo.zframework.M;
 import vo.zframework.anno.ZComponent;
 import vo.zframework.common.CU;
-import vo.zframework.common.STU;
-import vo.zframework.configuration.properties.ZMailNotificationConfigurationProperties;
 import vo.zframework.core.ZContext;
-import vo.zframework.email.ZMail;
-import vo.zframework.http.Task;
-import vo.zframework.http.request.ReqeustInfo;
 import vo.zframework.http.request.ZRequest;
 
 /**
@@ -43,61 +35,16 @@ public class ZControllerAdviceActuator {
 
 		LOG.error("执行异常,clientIp={},request=\r\n{}", clientIp, request.toHeaderString(), throwable);
 
-		final ZMailNotificationConfigurationProperties mn = ZContext.getBean(ZMailNotificationConfigurationProperties.class);
-		if (mn.getEnable() && CU.isNotEmpty(mn.getReceiver())
-				&& CU.isNotEmpty(mn.getMonitoredEvents())) {
-
-			final String message = Task.gExceptionMessage(throwable);
-
-			final ZMail mail = ZContext.getBean(ZMail.class);
-
-
-			for (final String event : mn.getMonitoredEvents()) {
-				if (message.contains(event)) {
-					final UUID eId = UUID.randomUUID();
-
-					// FIXME 2025年1月19日 下午6:41:44 zhangzhen : reqeust.body不要写入log，因其可能很大
-					LOG.error("邮件通知关注的事件[{}]发生了,事件id=[{}],request=[{}]", event, eId, request);
-
-					final String projectName = M.getAppName();
-
-					final String subject = "[" + projectName + "]工程里关注的事件[" + event + "]在机器["
-							+ M.getHostName() + "]上发生了";
-
-					// FIXME 2025年1月19日 下午7:17:23 zhangzhen : 考虑好：敏感信息要不要放入邮件的subject和body？
-					// 因为配置的邮箱可能不是自己的邮箱服务器
-					// 还是只写入日志，然后把eId放在邮件里通知一下让查看日志就好了？
-					final String body =
-							"request信息已写入LOG,请查看" + STU.CRLF
-									+ "事件ID:" + eId + STU.CRLF
-									+ STU.CRLF
-									+ "request信息:" + STU.CRLF + request + STU.CRLF
-									+ STU.CRLF
-									+ "事件详情:" + STU.CRLF + message + STU.CRLF
-									+ STU.CRLF
-									+ "发送时间:" + LocalDateTime.now() + STU.CRLF
-									;
-
-					for (final String receiver : mn.getReceiver()) {
-						mail.sendTextPlainAsync(subject, body, receiver);
-					}
-				}
-			}
-
-		}
-
 		final List<ZControllerAdviceBody> list = ZControllerAdviceScanner.LIST;
 		if (CU.isNotEmpty(list)) {
 			for (final ZControllerAdviceBody zcadto : list) {
 				if (zcadto.getThrowable().getCanonicalName().equals(throwable.getClass().getCanonicalName()) || ((throwable.getCause() != null) && zcadto.getThrowable().getCanonicalName()
 						.equals(throwable.getCause().getClass().getCanonicalName()))) {
-
 					try {
 						return zcadto.getMethod().invoke(zcadto.getObject(), throwable);
 					} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
 						e.printStackTrace();
 					}
-
 				}
 			}
 		}
