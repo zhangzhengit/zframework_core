30b90e0845491924488e0dc7091e64c60edfd22c
zhangzhen <1071324756@qq.com>
2026-08-04 03:30:14 +0800

    继续改为APT/maven编译期生成代码： ZAsyncRoute 完成

--- vortex-core/core/src/main/java/vo/vortex/scanner/ZAsyncScanner.java
+++ vortex-core/core/src/main/java/vo/vortex/scanner/ZAsyncScanner.java
@@ -2,29 +2,20 @@ package vo.vortex.scanner;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import vo.log.core.ZLog2;
 import vo.vortex.anno.ZAsync;
 import vo.vortex.anno.ZComponent;
 import vo.vortex.anno.ZService;
-import vo.vortex.aop.AOPParameter;
 import vo.vortex.aop.ZAsyncRV;
-import vo.vortex.common.RU;
 import vo.vortex.core.ZApplicationStartupInfo;
 import vo.vortex.core.ZContext;
 import vo.vortex.exception.StartupException;
 import vo.vortex.g.APT;
 import vo.vortex.g.G;
 import vo.vortex.route.IAsyncRoute;
-import vo.vortex.zclass.ZClass;
-import vo.vortex.zclass.ZMethod;
-import vo.vortex.zclass.ZMethodArg;
-import vo.vortex.zclass.ZPackage;
 
 /**
  * @ZAsync 启动流程
@@ -34,9 +25,8 @@ import vo.vortex.zclass.ZPackage;
  */
 public class ZAsyncScanner {
 
-	private static final ZLog2 LOG = ZLog2.getInstance();
+	private static final String CLASS_NAME = "vo.vortex.generated.maven.ZAsyncRoute";
 
-	private static final Class<ZAsync> TYPE = ZAsync.class;
 
 	public static Set<Class<?>> scan(final ZApplicationStartupInfo startupInfo) {
 
@@ -59,31 +49,10 @@ public class ZAsyncScanner {
 				.filter(cls -> cls.isAnnotationPresent(ZComponent.class) || cls.isAnnotationPresent(ZService.class))
 				.collect(Collectors.toSet()));
 
-		final ZClass proxyZClass = new ZClass();
-		proxyZClass.setPackage1(new ZPackage("vo.vortex.generated"));
-		proxyZClass.setName("ZAsyncRoute");
-		proxyZClass.setImplementsSet(Set.of(IAsyncRoute.class.getCanonicalName()));
-
-		final ZMethod routeMethod = new ZMethod();
-		routeMethod.setName("route");
-		routeMethod.setThrowsE(List.of(Exception.class.getCanonicalName()));
-		routeMethod.setReturnType(Object.class.getCanonicalName());
-
-		routeMethod.setMethodArgList(List.of(new ZMethodArg(AOPParameter.class.getCanonicalName(), "parameter")));
-
-		proxyZClass.setMethodSet(Set.of(routeMethod));
-
-
-		final StringBuilder routeBody = new StringBuilder();
-		routeBody.append("final String key = parameter.getSwitchValue();");
-		routeBody.append("switch (key) {");
-
-		int tI = 0;
-
 		for (final Class<?> cls : zcSet) {
 			final Method[] ms = cls.getDeclaredMethods();
 			for (final Method method : ms) {
-				final ZAsync async = method.getAnnotation(TYPE);
+				final ZAsync async = method.getAnnotation(ZAsync.class);
 				if (async == null) {
 					continue;
 				}
@@ -94,69 +63,11 @@ public class ZAsyncScanner {
 				if (mRT != Void.TYPE) {
 					vMRTIsZARV(cls, method, mRT);
 				}
-
-				final String clsname = cls.getCanonicalName();
-				final String methodName = method.getName();
-
-				final String parameterTL = Arrays.stream(method.getParameters()).map(p -> p.getType().getCanonicalName()).collect(Collectors.joining(",","\"","\""));
-
-				final String value = clsname + "." + methodName + "." + parameterTL.replace("\"", "");
-
-				tI++;
-
-				routeBody.append("case ")
-						.append("\"").append(value).append("\":");
-
-				routeBody
-				.append(cls.getCanonicalName()).append(" target").append(tI).append(" = ")
-				.append("(").append(cls.getCanonicalName()).append(")")
-				.append(ZContext.class.getCanonicalName()).append(".getBean")
-				.append("(\"").append(cls.getCanonicalName()).append(".original\");");
-
-				final Class<?>[] pt = method.getParameterTypes();
-				final StringBuilder b = new StringBuilder();
-				for (int i = 0;i<pt.length;i++) {
-
-					b
-					.append("(")
-					.append(RU.ptToBox(pt[i].getTypeName()))
-					.append(")parameter.getParameterList().get(").append(i).append(")");
-
-					if(i < (pt.length - 1)) {
-						b.append(",");
-					}
-				}
-
-				final Class<?> returnType = method.getReturnType();
-				final boolean isVoid = returnType.getCanonicalName() == void.class.getCanonicalName();
-				if (!isVoid) {
-					routeBody.append("return ");
-				}
-
-				routeBody.append("target")
-				.append(tI).append('.')
-				.append(methodName)
-				.append("(")
-				.append(b)
-				.append(");")
-				;
-
-				if (isVoid) {
-					routeBody.append("break;");
-				}
 			}
 		}
 
-		routeBody.append("default:\r\n"
-				+ "	break;\r\n"
-				+ "}	");
-
-		routeMethod.setBody(routeBody.toString());
-
-//		System.out.println("proxyZClass = ");
-//		System.out.println(proxyZClass.toString());
-
-		ZContext.addBeanAsync(IAsyncRoute.class, () -> proxyZClass.newInstance());
+		final Object proxyZClassInstance = G.newInstance(G.load(CLASS_NAME));
+		ZContext.addBean(IAsyncRoute.class, proxyZClassInstance);
 
 		return zcSet;
 	}
@@ -165,7 +76,7 @@ public class ZAsyncScanner {
 		if (mRT != ZAsyncRV.class) {
 			final String me =
 					"\r\n\t"
-					+ "@" + TYPE.getSimpleName()
+					+ "@" + ZAsync.class.getSimpleName()
 					+ "方法声明错误："
 					+ "\r\n\t"
 					+ cls.getName() + "." + method.getName()
@@ -177,7 +88,7 @@ public class ZAsyncScanner {
 					+ "请修改代码："
 					+ "\r\n\t"
 					+ "1、删除方法上的@"
-					+ TYPE.getSimpleName()
+					+ ZAsync.class.getSimpleName()
 					+ "\r\n\t"
 					+ "2、修改返回类型为[" + ZAsyncRV.class.getSimpleName() + "<"+mRT.getSimpleName()+">]的形式,"
 					+ "并且使用"+ZAsyncRV.class.getSimpleName()+".ok(final T v)方法来构造返回值"
@@ -191,16 +102,16 @@ public class ZAsyncScanner {
 		if (!Modifier.isPublic(method.getModifiers())) {
 			final String me =
 					"\r\n\t"
-					+ "@" + TYPE.getSimpleName()
+					+ "@" + ZAsync.class.getSimpleName()
 					+ "方法声明错误："
 					+ "\r\n\t"
 					+ cls.getName() + "." + method.getName()
 					+ "\r\n\t"
-					+ "带有@" + TYPE.getSimpleName() + "的方法必须声明为public"
+					+ "带有@" + ZAsync.class.getSimpleName() + "的方法必须声明为public"
 					+ "\r\n\t"
 					+ "请修改代码："
 					+ "\r\n\t"
-					+ "1、去除@" + TYPE.getSimpleName()
+					+ "1、去除@" + ZAsync.class.getSimpleName()
 					+ "\r\n\t"
 					+ "2、修改方法为public"
 					+ "\r\n\t"
